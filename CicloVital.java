a87711f46120625e6a50db1b92ae7838d7523727
gello <amanuel@gmail.com>
2025-05-21 14:41:49 +0200

    Hola muy buenas

--- CicloVital.java
+++ CicloVital.java
@@ -2,7 +2,7 @@ public class CicloVital {
 
     private static final int NATALIDAD_MAXIMA = 1;
     private static final int VITALIDAD_MAXIMA = 2;
-
+    //hola muy buenas
     public void reproducir(){
 
     }
